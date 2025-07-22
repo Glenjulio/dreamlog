9a29876b3cf3e02eb3f7c309ff1f68aa038aa1bd
Feret Thomas <thomas.feret@hotmail.fr>
2025-07-22 22:21:16 +0200

    forgot-#-in-a-comment-inside-dreams-controller

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -1,5 +1,5 @@
 class DreamsController < ApplicationController
-  #before_action :authenticate_user!
+  before_action :authenticate_user!
   before_action :set_dream, only: [:show, :edit, :update, :destroy, :transcribe]
 
   def mydreams
